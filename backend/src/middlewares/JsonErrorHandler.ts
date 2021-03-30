df6581e17fb435c0c5c989ecdc2c8ed948ac2bd9
Angel S. Moreno <angelxmoreno@gmail.com>
2021-03-29 21:09:59 -0400

    improved consistent error response

--- backend/src/middlewares/JsonErrorHandler.ts
+++ backend/src/middlewares/JsonErrorHandler.ts
@@ -1,51 +1,13 @@
 import { ExpressErrorMiddlewareInterface, Middleware } from 'routing-controllers';
 import { Request, Response } from 'express';
-
-interface ErrorWithStatus extends Error {
-  status?: number;
-  code?: number;
-}
-
-interface ErrorContainer {
-  errors: ((ErrorWithStatus & ErrorContainer) | string)[];
-}
-
-interface ErrorPayload {
-  status: number;
-  message: string;
-  type?: string;
-  errors?: ErrorPayload[];
-}
-
-const normalizeError = (error: (ErrorWithStatus & ErrorContainer) | string): ErrorPayload => {
-  let status = 0;
-  let message = 'Unknown Error';
-  let errors;
-  let type: string | undefined;
-  if (typeof error === 'string') {
-    status = 500;
-    message = error;
-  } else if (error.message) {
-    status = error.status || 500;
-    message = error.message;
-    type = error.name;
-  } else if (error.errors) {
-    status = 500;
-    message = 'Multiple Errors';
-    errors = error.errors.map(row => normalizeError(row));
-    if (errors.length === 1) {
-      return errors[0];
-    }
-  }
-
-  return { status, message, errors, type };
-};
+import normalizeError, { GenericError } from '../helpers/normalizeError';
 
 @Middleware({ type: 'after' })
 export class JsonErrorHandler implements ExpressErrorMiddlewareInterface {
-  error(error: (ErrorWithStatus & ErrorContainer) | string, request: Request, response: Response): void | Response {
-    console.log('error found', typeof error);
+  error(error: GenericError | string, request: Request, response: Response): void | Response {
+    console.error('error found', typeof error);
     const errorPayload = normalizeError(error);
+    console.error('errorPayload', errorPayload);
     response.status(errorPayload.status).json(errorPayload).end();
   }
 }
--- backend/src/middlewares/NotFoundMiddleware.ts
+++ backend/src/middlewares/NotFoundMiddleware.ts
@@ -1,11 +1,14 @@
-import { ExpressMiddlewareInterface, Middleware } from 'routing-controllers';
+import { ExpressMiddlewareInterface, Middleware, NotFoundError } from 'routing-controllers';
 import { Request, Response } from 'express';
+import normalizeError from '../helpers/normalizeError';
 
 @Middleware({ type: 'after' })
 export class NotFoundMiddleware implements ExpressMiddlewareInterface {
   public use(req: Request, res: Response): void {
     if (!res.headersSent) {
-      res.status(404).json({ code: 404, error: 'Path not found', method: req.method, path: req.path });
+      const payload = normalizeError(new NotFoundError(`${req.method} ${req.path} not found`));
+      console.log('payload', payload);
+      res.status(404).json(payload);
     }
     res.end();
   }
