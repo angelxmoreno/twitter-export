eb7811a56a46bd4b5b3b085bdd8b524314d92138
Angel S. Moreno <angelxmoreno@gmail.com>
2021-03-31 02:56:07 -0400

    added base pagination to models

--- backend/src/repositories/RepositoryBase.ts
+++ backend/src/repositories/RepositoryBase.ts
@@ -3,8 +3,29 @@ import { FindConditions } from 'typeorm/find-options/FindConditions';
 import { FindOneOptions } from 'typeorm/find-options/FindOneOptions';
 import { DeepPartial } from 'typeorm/common/DeepPartial';
 import { SaveOptions } from 'typeorm/repository/SaveOptions';
+import buildPaginator from 'pagination-apis';
+
+type PaginateOptions = {
+  page: number;
+  limit: number;
+  maximumLimit: number;
+};
+
+interface Paginate {
+  data: Array<unknown>;
+  total: number;
+  totalPages: number;
+  previous?: string;
+  next?: string;
+}
 
 export default class RepositoryBase<Entity> extends AbstractRepository<Entity> {
+  paginationOptions: PaginateOptions = {
+    page: 1,
+    limit: 20,
+    maximumLimit: 100,
+  };
+
   findOne(conditions?: FindConditions<Entity>, options?: FindOneOptions<Entity>): Promise<Entity | undefined> {
     return this.repository.findOne(conditions, options);
   }
