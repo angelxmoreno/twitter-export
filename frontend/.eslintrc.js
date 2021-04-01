1546d9feb4b2419e7318ff7961dca20d75df9c58
Angel S. Moreno <angelxmoreno@gmail.com>
2021-04-01 18:09:48 -0400

    created react app with eslint+prettier+TS settings

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['json-format', 'react', '@typescript-eslint', 'jest', 'prettier'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    Atomics: 'readonly',
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    extraFileExtensions: ['.json', '.css'],
+  },
+  rules: {
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+        printWidth: 120,
+      },
+    ],
+  },
+};
