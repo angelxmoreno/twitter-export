0ba9d5d21645c5593cf8ef176286a25e97000461
Angel S. Moreno <angelxmoreno@gmail.com>
2021-04-03 02:03:04 -0400

    Fixed redisUrl not defined error

--- backend/src/services/Cache/redis.ts
+++ backend/src/services/Cache/redis.ts
@@ -2,5 +2,5 @@ import { Cache } from 'cache-manager';
 import Env from '../../helpers/Env';
 import redisCacheFromUrl from '../../helpers/redisCacheFromUrl';
 
-const redisUrl = Env.string('REDIS_URL', undefined);
+const redisUrl = Env.string('REDIS_URL', '');
 export const redisCache: Cache | undefined = redisUrl ? redisCacheFromUrl(redisUrl, 60) : undefined;
